164bb26adfa018f1f59ca01197144d922dc2b127
Timo Nicolai <timonicolai@arcor.de>
2019-05-19 23:43:02 +0200

    Implement halt, out, noop

--- src/vm.js
+++ src/vm.js
@@ -9,6 +9,11 @@ module.exports = {
 function Vm() {
     this.mem = null;
     this.pc = 0;
+
+    this.opcodes = {
+        19: this.out,
+        21: this.noop,
+    }
 }
 
 Vm.prototype.load = function(file) {
@@ -19,8 +24,29 @@ Vm.prototype.run = function() {
     while (this.pc <= this.mem.maxAddr) {
         var op = this.mem.read(this.pc);
 
-        // TODO
+        if (op == 0)
+            break;
+
+        if (!(op in this.opcodes))
+            throw new Error(`invalid opcode: ${op}`);
+
+        // find instruction
+        var f = this.opcodes[op];
+
+        // read arguments
+        var args = [];
+        for (var i = 0; i < f.length; ++i)
+            args.push(this.mem.read(++this.pc));
+
+        // run instruction
+        f(...args);
 
         ++this.pc;
     }
 }
+
+Vm.prototype.out = function(c) {
+    process.stdout.write(String.fromCharCode(c));
+}
+
+Vm.prototype.noop = function() {}
