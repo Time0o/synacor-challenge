8c0f3bb08bdd4b019399c378dc46270476ec428f
Timo Nicolai <timonicolai@arcor.de>
2019-05-24 18:53:34 +0200

    Add nodes to map

--- map/map.js
+++ map/map.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var cytoscape = require('cytoscape');
-var cycola = require('cytoscape-cola');
+var cose = require('cytoscape-cose-bilkent');
 
-cytoscape.use(cycola);
+cytoscape.use(cose);
 
 var cy = cytoscape({
     container: document.getElementById('cy'),
@@ -35,7 +35,65 @@ var cy = cytoscape({
 });
 
 var adj = {
-    // TODO
+    'Foothills (1)': [
+        ['doorway', 'Dark cave (1)'],
+        ['south', 'Foothills (2)']
+    ],
+    'Foothills (2)': [
+        ['north', 'Foothills (1)']
+    ],
+    'Dark cave (1)': [
+        ['south', 'Foothills (1)'],
+        ['north', 'Dark cave (2)']
+    ],
+    'Dark cave (2)': [
+        ['south', 'Dark cave (1)'],
+        ['north', 'Dark cave (3)']
+    ],
+    'Dark cave (3)': [
+        ['south', 'Dark cave (2)'],
+        ['bridge', 'Rope bridge (1)']
+    ],
+    'Rope bridge (1)': [
+        ['back', 'Dark cave (3)'],
+        ['continue', 'Falling through the Air!']
+    ],
+    'Falling through the Air!': [
+        ['down', 'Moss cavern (1)']
+    ],
+    'Moss cavern (1)': [
+        ['west', 'Moss cavern (2)'],
+        ['east', 'Moss cavern (3)']
+    ],
+    'Moss cavern (2)': [
+        ['east', 'Moss cavern (1)'],
+        ['passage', 'Passage (1)']
+    ],
+    'Moss cavern (3)': [
+        ['west', 'Moss cavern (1)']
+    ],
+    'Passage (1)': [
+        ['cavern', 'Moss cavern (2)'],
+        ['ladder', 'Twisty passages (1)'],
+        ['darkness', 'Passage (2)']
+    ],
+    'Passage (2)': [
+        ['back', 'Passage (1)'],
+        ['continue', 'Fumbling around in the darkness (1)']
+    ],
+    'Fumbling around in the darkness (1)': [
+        ['forward', 'Fumbling around in the darkness (2)'],
+        ['back', 'Fumbling around in the darkness (2)']
+    ],
+    'Fumbling around in the darkness (2)': [
+        ['run', 'Panicked and lost'],
+        ['investigate', 'Panicked and lost']
+    ],
+    'Panicked and lost': [
+        ['run', 'You have been eaten by a grue'],
+        ['wait', 'You have been eaten by a grue'],
+        ['hide', 'You have been eaten by a grue']
+    ],
 };
 
 function addNode(node) {
@@ -67,4 +125,4 @@ for (var from in adj) {
     });
 }
 
-cy.layout({ name: 'cola', nodeSpacing: 100 }).run();
+cy.layout({ name: 'cose-bilkent', idealEdgeLength: 100 }).run();
